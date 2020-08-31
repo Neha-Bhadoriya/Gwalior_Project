e089a5d4f5130faa761befadab02866c988a0b6a
Neha-Bhadoriya <nehabhadoriya33@gmail.com>
2020-08-30 17:17:01 -0700

    Gwalior Project

---
+++ get.js
@@ -0,0 +1,11 @@
+var element;
+
+element =document.body;
+
+// element=document.getElementById("vikas").innerText;
+// element=document.getElementById("neha").innerHTML;
+// element=document.getElementById("neha").getAttribute("style");
+// element=document.getElementById("neha").getAttributeNode("style");
+// element=document.getElementById("header").attributes;
+element=document.getElementById("header").attributes[0];
+console.log(element);
---
+++ set.js
@@ -0,0 +1,12 @@
+
+var element;
+document.getElementById("a").innerHTML="<center><h1>ITM COLLEGE</h1></center>"
+// document.getElementById("a").innerHTML="<center><h1>ITM COLLEGE</h1></center>"
+document.getElementById("a").setAttribute("style","border:10px dotted yellow");
+// document.getElementById("a").removeAttribute("style");
+// document.getElementById("a").innerHTML="<center><h1>ITM COLLEGE</h1></center>"
+
+// query
+// document.querySelector("#a").innerHTML="<center><h1>WoW</h1></center>";
+//  element=document.querySelector("#a");
+// console.log(element);
